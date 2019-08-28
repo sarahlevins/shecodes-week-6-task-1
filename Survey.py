235df1543f23ba74859b7af3f2da46c4abde3f8b
sarahlevins <sarahjeanlevins@gmail.com>
2019-08-28 16:32:12 +0800

    working with a dictionary. start

--- Survey.py
+++ Survey.py
@@ -1,14 +1,18 @@
 from Question import Question
+from Answer import Answer
+from Section import Section
 
 class Survey:
+    def __init__(self,survey_sections):
+        self.survey_sections = survey_sections
 
     def start_survey():
         print ('Welcome to my She Codes Survey!')
         print ('it is nice to meet you')
         name = input('What is your name? ')
         return name
-    
-    
+            
+
 
     # def start_survey_sections()
 
--- shecodes_survey.py
+++ shecodes_survey.py
@@ -6,27 +6,6 @@ from Answer import Answer
 # Welcome User to Survey
 Survey.start_survey()
 
-# 1 - ask if they are a student or a mentor
-
-# qualify_one = Question('Are you a student or a mentor? ')
-# qualify_two = Question('How many classes have you attended? ')
-# qualify_three = Question('Are you comfortable using skill social? ')
-# qualify_four = Question('Are you comfortable using slack? ')
-
-# answer_one = (qualify_one.ask_question('Are you a student or a mentor?'))
-# answer_two = Question(qualify_two.ask_question('How many classes have you attended?'))
-# answer_three = Question(qualify_three.ask_question('Are you comfortable using skill social? '))
-# answer_four = Question(qualify_four.ask_question('Are you comfortable using slack?'))
-
-# print (answer_one)
-# print (answer_two)
-# print (answer_three)
-# print (answer_four)
-answer_options_list = ['A', 'B']
-# question_one = Question('This is your first question', answer_options_list)
-
-# question_one.ask_question()
-
 
 #STUDENT OR MENTOR QUESTION SECTION
 student_or_mentor = Section(
@@ -34,42 +13,58 @@ student_or_mentor = Section(
     Question(
         'Are you a student or mentor?', 
         ['[A] - Yes, I am a student', 
-         '[B] - No I am not a student']),
+         '[B] - No, I am not a student']),
     #first_question
-    Question ('Do you like being a student?', answer_options_list),
+    Question ('Do you like being a student?', 
+        ['[A] - Yes',
+         '[B] - No']),
     #description
     'This section will ask you about being a shecodes student'
 )
 
+#SKILLSOCIAL SECTION
+skill_social = Section(
+    #qualifying_question
+    Question(
+        'Have you used skill social before?', 
+        ['[A] - Yes', 
+         '[B] - No']),
+    #first_question
+    Question ('Do you like skill social', 
+        ['[A] - Yes',
+         '[B] - No']),
+    #description
+    'This section is about using skill social'
+)
+
+#SKILLSOCIAL SECTION
+slack = Section(
+    #qualifying_question
+    Question(
+        'Have you used slack before?', 
+        ['[A] - Yes', 
+         '[B] - No']),
+    #first_question
+    Question ('Do you like slack', 
+        ['[A] - Yes',
+         '[B] - No']),
+    #description
+    'This section is about using slack'
+)
+
+section_dictionary = {
+    1 : student_or_mentor,
+    2 : skill_social,
+    3 : slack
+}
+
+# ASK QUALIFYING QUESTION ONE, take answer and return true or false
 print(student_or_mentor.description)
-print(student_or_mentor.qualifying_question)
-
-my_answer = Answer('A', 'This is my answer', Question('this is a question', answer_options_list), True)
-
-# ASK QUALIFYING QUESTION ONE
+print(f'{student_or_mentor.qualifying_question.question_text}')
 print(f'{student_or_mentor.qualifying_question.answers[0]} ')
 print(f'{student_or_mentor.qualifying_question.answers[1]} ')
-student_or_mentor.qualifying_question.get_answer_by_code()
+# do method to get true or false
+is_student = student_or_mentor.qualifying_question.get_answer_by_code()
 
-
-
-#QUALIFYING QUESTIONS
-
-    # SECTION 1 (OBJECT) 
-        # QUESTION  - QUALIFYING QUESTION
-
-    # SECTION 2 (QUESTION) - QUESTION 2
-
-    # OBJECT (QUESTION) - QUESTION 3 
-
-#OBJECT (SECTION) - SECTION 1 - STUDENT QUESTIONS
-
-    # OBJECT (QUESTION) - QUESTION 1
-
-    # OBJECT (QUESTION) - QUESTION 2
-
-
-#OBJECT (SECTION) SECTION 3 - IN CLASS QUESTIONS
-
-#SECTION 4 - SLACK QUESTIONS
+print(f'{section_dictionary[2].qualifying_question.question_text}')
 
